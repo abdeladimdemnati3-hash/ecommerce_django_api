33cf6fc4d2c126664efdeca2160d55e584eba582
Abdeladim <abdeladimdemnati3@gmail.com>
2025-10-14 14:15:00 +0100

    Modeling Our Data

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 
 # Create your views here.
 @api_view(['GET'])
@@ -16,17 +15,19 @@ def getRoutes(request):
         '/api/products/<id>/',
         '/api/products/delete/<id>/',
         '/api/products/<update>/<id>/',
-        ]
+    ]
     return Response(routes)
+
 
 @api_view(['GET'])
 def getProducts(request):
     return Response(products)
-    
+
+
 @api_view(['GET'])
 def getProduct(request, id):
     product = next((item for item in products if item["_id"] == id), None)
     if product:
         return Response(product)
     else:
-        return Response({'message': 'Product not found'}, status=404)        
+        return Response({'message': 'Product not found'}, status=404)
